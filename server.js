a66ccc1404f4c41b4d1ecaf351eaeb9763dfaf03
Johnathan Osorno Valencia <jnosornov@gmail.com>
2020-10-09 14:15:00 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello Stranger')
+})
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}`)
+})
